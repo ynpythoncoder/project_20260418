bba679c61fee730d9222be1b4d3af66f3d2bc57f
Yusaku Numata <yusaku.numata@unrou.biz>
2026-05-03 18:12:11 +0900

    起動確認済み

--- core/settings.py
+++ core/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',#
 
-    'tools.developments.apps.DevelopmentsConfig',
 ] + INSTALLED_APPS_AUTOGEN
 
 STRUCTURE_CONCRETE_LAYER_NAMES = {
@@ -110,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
